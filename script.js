752bcd491e8433fdc0928e1d6c88e9fe0fcd3617
TGeorge <60828138+TLGeorge@users.noreply.github.com>
2020-03-13 14:52:49 -0400

    Added variables for character choices, added prompts, added some draft if statements, added event listener to the generate button.

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var newPassword =
+// Add event listener to generate button
+generateBtn.addEventListener("click", function () {
+    var passwordLength = Number(prompt("How many characters would you like your password to contain? Please enter a number between 8 and 128."));
+    var lowerPassword = prompt("Would You like your password to include lowercase letters? Please enter 'Yes' or 'No'.");
+    var upperPassword = prompt("Would You like your password to include uppercase letters? Please enter 'Yes' or 'No'.");
+    var numPassword = prompt("Would You like your password to include number? Please enter 'Yes' or 'No'.");
+    var specialPassword = prompt("Would You like your password to include uppercase letters? Please enter 'Yes' or 'No'.");
+})
 
-    // Function for returning Password to end-user
-    function generatePassword() {
-        return 'password that was generated'
-    }
+// var securePassword 
+
+// var passwordLength = Number(prompt("How many characters would you like your password to contain? Please enter a number between 8 and 128."));
+// var lowerPassword = prompt("Would You like your password to include lowercase letters? Please enter 'Yes' or 'No'.");
+// var upperPassword = prompt("Would You like your password to include uppercase letters? Please enter 'Yes' or 'No'.");
+// var numPassword = prompt("Would You like your password to include number? Please enter 'Yes' or 'No'.");
+// var specialPassword = prompt("Would You like your password to include uppercase letters? Please enter 'Yes' or 'No'.");
+
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGJKLMNPRSTUVWXYZ";
+var numeric = "0123456789";
+var special = "!@#$%&*()_+-=[]|,./?><";
+var allAllowed = "abcdefghijklmnopqrstuvwxyzABCDEFGJKLMNPRSTUVWXYZ0123456789!@#$%&*()_+-=[]|,./?><";
 
 
+// Function for returning Password to end-user
+function generatePassword() {
+    return 'password that was generated'
+}
+
+if (passwordLength >= 8 && passwordLength <= 128) {
+
+}
+
+if (lowerPassword === 'Yes' || lowerPassword === 'yes') {
+
+}
+
+if (numPassword === 'Yes' || numPassword === 'yes') {
+
+}
+
+if (specialPassword === 'Yes' || numPassword === 'yes') {
+
+}
 
 
 // Write password to the #password input
