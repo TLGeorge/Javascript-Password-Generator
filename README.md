a1c0c66599a0ef2bab61f9e8624ee396fdd761e8
TGeorge <60828138+TLGeorge@users.noreply.github.com>
2020-03-14 14:44:01 -0400

    updated readme, if statements, changed global scope of variables.

--- README.md
+++ README.md
@@ -1,32 +1,5 @@
 # Javascript-Password-Generator
 
-## Acceptance Criteria
-
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
-
-The following image demonstrates the application functionality:
-
-![password generator demo](./Assets/03-javascript-homework-demo.png)
-
-
-review activity 20 - hard loop
-
 1. Prompt user to add a desired password length between 8-128.
     * If the user selects a valid password length, then they will be prompted to verify which criteria they want included in their secure password. 
 
@@ -43,3 +16,11 @@ review activity 20 - hard loop
 
 3. After all the criteria is picked, random values from each are added to the secure password.
 4. The secure password is then displayed to the user. 
+
+* Taneisha's Password Generator 
+https://tlgeorge.github.io/Javascript-Password-Generator/
+
+* Taneisha's Password Generator GitHub Repository
+
+https://github.com/TLGeorge/Javascript-Password-Generator
+
--- script.js
+++ script.js
@@ -1,86 +1,80 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", function () {
-    var passwordLength = prompt("How many characters would you like your password to contain? Please enter a number between 8 and 128.");
+var passwordLength = "0"
+var lowerPassword = '';
+var upperPassword = '';
+var numPassword = '';
+var specialPassword = '';
+
+//Convert user response to lower case so it will match 'y' or 'n'.
+var lowerPassword = '';
+var upperPassword = '';
+var numPassword = '';
+var specialPassword = '';
+
+// Function for generating Password to meet requirements set by end-user. If prompt answer is yes, add a random character from that set of options to the password.
+
+function generatePassword() {
+
+    passwordLength = parseInt(prompt("How many characters would you like your password to contain? Please enter a number between 8 and 128."));
     // Validate that user enters valid character length.
-    if (passwordLength < 8 || passwordLength > 128) {
+    if (passwordLength <= 8 || passwordLength >= 128) {
         alert("Password must be a minimum of 8 characters and a maximum of 128");
         prompt("How many characters would you like your password to contain? Please enter a number between 8 and 128.")
     }
-    var lowerPassword = prompt("Would You like your password to include lowercase letters? Please enter 'y' or 'n'.");
-    var upperPassword = prompt("Would You like your password to include uppercase letters? Please enter 'y' or 'n'.");
-    var numPassword = prompt("Would You like your password to include number? Please enter 'y' or 'n'.");
-    var specialPassword = prompt("Would You like your password to include uppercase letters? Please enter 'y' or 'n'.");
+    lowerPassword = prompt("Would You like your password to include lowercase letters? Please enter 'y' or 'n'.");
+    upperPassword = prompt("Would You like your password to include uppercase letters? Please enter 'y' or 'n'.");
+    numPassword = prompt("Would You like your password to include number? Please enter 'y' or 'n'.");
+    specialPassword = prompt("Would You like your password to include uppercase letters? Please enter 'y' or 'n'.");
 
-    //Convert user response to lower cases
-    var lowerPassword = lowerPassword.toLowerCase();
-    var upperPassword = upperPassword.toLowerCase();
-    var numPassword = numPassword.toLowerCase();
-    var specialPassword = specialPassword.toLowerCase();
+    //Convert user response to lower case so it will match 'y' or 'n'.
+    lowerPassword = lowerPassword.toLowerCase();
+    upperPassword = upperPassword.toLowerCase();
+    numPassword = numPassword.toLowerCase();
+    specialPassword = specialPassword.toLowerCase();
 
     // Validate that at least one of the password criteria were chosen.
-    if (lowerPassword === 'n' || upperPassword === 'n' || numPassword === 'n' || specialPassword === 'n') {
+    if (lowerPassword === 'n' && upperPassword === 'n' && numPassword === 'n' && specialPassword === 'n') {
         alert("Password must contain one of the given criteria of lowercase, uppercase, numeric or special. Select 'Generate Password' to try again.")
     }
-})
-
-// Character Sets (password criteria) that end-users choose (via prompts) to include or omit from their password; lower, upper, numeric and special.
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGJKLMNPRSTUVWXYZ";
-var numeric = "0123456789";
-var specialChar = "!@#$%&*()_+-=[]|,./?><";
-
-// // Password created from selected criteria
-// var securePassword = '';
 
 
-// Function for generating Password to meet requirements set by end-user
-// function generatePassword(passwordLength) {
-//     var securePassword = '';
-//     for (var i = 0; i < passwordLength; i++) {
-//         securePassword + (lowercaseAt(Math.floor(Math.random()) * passwordLength))
+    var password = '';
+    // Character Sets (password criteria) that end-users choose (via prompts) to include or omit from their password; lower, upper, numeric and special.
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    var uppercase = "ABCDEFGJKLMNPRSTUVWXYZ";
+    var numeric = "0123456789";
+    var specialChar = "!@#$%&*()_+-=[]|,./?><";
 
+    console.log("hi")
 
-// function appendSecurePassword
+    for (var i = 0; i < passwordLength; i++) {
 
-// Function for generating Password to meet requirements set by end-user
-// function generatePassword(passwordLength) {
-//     var password = '';
-//     for (var i = 0; i < passwordLength; i++) {
-//         securePassword + (lowercaseAt(Math.floor(Math.random()) * passwordLength))
+        if (lowerPassword === 'y' && passwordLength >= 8 || passwordLength <= 128) {
+            console.log(password += lowercase.charAt(Math.floor(Math.random) * lowercase.length));
+        }
 
+        if (upperPassword === 'y' && passwordLength >= 8 || passwordLength <= 128) {
+            console.log(password += uppercase.charAt(Math.floor(Math.random) * uppercase.length));
+        }
 
-//         if (passwordLength < 8 || passwordLength > 128) {
-//             alert("Password must be a minimum of 8 characters and a maximum of 128");
-//         else {
+        if (numPassword === 'y' && passwordLength >= 8 || passwordLength <= 128) {
+            console.log(password += numeric.charAt(Math.floor(Math.random) * numeric.length));
+        }
 
-//             }
-//         }
-//         // Validate that user enters valid character length.
-//         else {
-//             alert("Password must be a minimum of 8 characters and a maximum of 128");
-//         }
+        if (specialPassword === 'y' && passwordLength >= 8 || passwordLength <= 128) {
+            console.log(password += specialChar.charAt(Math.floor(Math.random) * specialChar.length));
+        }
+    }
+    return password
+}
 
-//         if (lowerPassword === 'y') {
-
-//         }
-
-//         if (numPassword === 'y') {
-
-//         }
-
-//         if (specialPassword === 'y') {
-
-//         }
-//         for (var i = 0; i < passwordLength; i++) {
-//             var character = Math.floor(Math.random() * passwordLength) + 1;
-//         }
-
-//         return securePassword
-//     }
-
+// first if statement, if yes to lowercase, add lowercase random letter to securePassword.
+// Same for every other prompt.
+// securePasssword might be 1 or 4 characters long depending on prompt answers
+// passwordLength - securepasword.length
+// */
 
 
 
@@ -92,3 +86,5 @@ function writePassword() {
     passwordText.value = password;
 
 }
+
+generateBtn.addEventListener("click", writePassword);
