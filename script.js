21b99fa0b8b18c32ae930a5eb87343c7c8df4435
TGeorge <60828138+TLGeorge@users.noreply.github.com>
2020-03-15 01:34:26 -0400

    Updated if statements. Password generator now works for final password to match user's input.

--- script.js
+++ script.js
@@ -1,35 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var passwordLength = "0"
-var lowerPassword = '';
-var upperPassword = '';
-var numPassword = '';
-var specialPassword = '';
-
-var lowerPassword = '';
-var upperPassword = '';
-var numPassword = '';
-var specialPassword = '';
-
 // Function for generating Password to meet requirements set by end-user. If prompt answer is yes, add a random character from that set of options to the password.
-
 function generatePassword() {
+    var securePassword = "";
+    var passwordSet = "";
 
-    passwordLength = parseInt(prompt("How many characters would you like your password to contain? Please enter a number between 8 and 128."));
-    // Validate that user enters valid character length.
-    if (passwordLength < 8 || passwordLength > 128) {
-        alert("Password must be a minimum of 8 characters and a maximum of 128");
-        prompt("How many characters would you like your password to contain? Please enter a number between 8 and 128.")
-    }
+    var passwordLength = "";
+    var passwordValue = "";
+
+    inputLength = parseInt(prompt("How many characters would you like your password to contain? Please enter a number between 8 and 128."));
     lowerPassword = prompt("Would You like your password to include lowercase letters? Please enter 'y' or 'n'.");
     upperPassword = prompt("Would You like your password to include uppercase letters? Please enter 'y' or 'n'.");
     numPassword = prompt("Would You like your password to include number? Please enter 'y' or 'n'.");
     specialPassword = prompt("Would You like your password to include uppercase letters? Please enter 'y' or 'n'.");
-    // Validate that at least one of the password criteria were chosen.
-    if (lowerPassword === 'n' && upperPassword === 'n' && numPassword === 'n' && specialPassword === 'n') {
-        alert("Password must contain one of the given criteria of lowercase, uppercase, numeric or special. Select 'Generate Password' to try again.")
-    }
 
     //Convert user response to lower case so it will match 'y' or 'n'.
     lowerPassword = lowerPassword.toLowerCase();
@@ -37,37 +21,50 @@ function generatePassword() {
     numPassword = numPassword.toLowerCase();
     specialPassword = specialPassword.toLowerCase();
 
-
-    var password = '';
     // Character Sets (password criteria) that end-users choose (via prompts) to include or omit from their password; lower, upper, numeric and special.
     var lowercase = "abcdefghijklmnopqrstuvwxyz";
     var uppercase = "ABCDEFGJKLMNPRSTUVWXYZ";
     var numeric = "0123456789";
     var specialChar = "!@#$%&*()_+-=[]|,./?><";
 
-
-    for (var i = 0; i < passwordLength; i++) {
-
-        if (lowerPassword === 'y' && passwordLength >= 8 || passwordLength <= 128) {
-            console.log(password += lowercase.charAt(Math.floor(Math.random) * lowercase.length));
-        }
-
-        if (upperPassword === 'y' && passwordLength >= 8 || passwordLength <= 128) {
-            console.log(password += uppercase.charAt(Math.floor(Math.random) * uppercase.length));
-        }
-
-        if (numPassword === 'y' && passwordLength >= 8 || passwordLength <= 128) {
-            console.log(password += numeric.charAt(Math.floor(Math.random) * numeric.length));
-        }
-
-        if (specialPassword === 'y' && passwordLength >= 8 || passwordLength <= 128) {
-            console.log(password += specialChar.charAt(Math.floor(Math.random) * specialChar.length));
-        }
+    // Takes value that user has input for desired length of password and starts to generate 
+    if (inputLength >= 8 && inputLength <= 128) {
+        passwordValue = inputLength;
+    } else {
+        // Validate that user enters valid character length.
+        return ("Password must be a minimum of 8 characters and a maximum of 128!")
     }
-    return password
+
+    // If prompt answer is yes, add a random character from that set of options to the password.
+    if (upperPassword === "y") {
+        passwordSet += uppercase;
+    }
+
+    if (lowerPassword === "y") {
+        passwordSet += lowercase;
+    }
+
+    if (numPassword === "y") {
+        passwordSet += numeric;
+    }
+
+    if (specialPassword === "y") {
+        passwordSet += specialChar;
+    }
+
+    // Validate that at least one of the password criteria were chosen.
+    else if (lowerPassword === 'n' && upperPassword === 'n' && numPassword === 'n' && specialPassword === 'n') {
+        alert("Password must contain one of the given criteria of lowercase, uppercase, numeric or special. Select 'Generate Password' to try again.");
+    }
+    // Adjusts length of password from user's criteria to length that they desire (via first prompt).
+    passwordLength = passwordValue;
+
+    for (let i = 0; i < passwordLength; i++) {
+        securePassword += passwordSet.charAt(
+            Math.floor(Math.random() * passwordSet.length));
+    }
+    return securePassword;
 }
-
-
 
 
 
